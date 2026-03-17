2e08ca540d8f983cc6a2191fd940a40dc60d1e01
Dan Morgan <dan@dmorgan.xyz>
2026-03-17 22:25:47 +0000

    Initial Commit

---
+++ MacAMRPTests/MacAMRPTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacAMRPTests.swift
+//  MacAMRPTests
+//
+//  Created by Dan Morgan on 17/03/2026.
+//
+
+import Testing
+@testable import MacAMRP
+
+struct MacAMRPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
